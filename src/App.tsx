50835363430877738214aef135e4e1e7779bf826
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2022-12-16 12:27:58 -0300

    feat: configuração do Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
-import {
-  Heading_1_700,
-  Heading_2_600,
-  Heading_3_600,
-  Heading_4_500,
-  Heading_4_600,
-  Heading_5_500,
-  Heading_5_600,
-  Heading_6_500,
-  Heading_6_600,
-  Heading_7_500,
-  Heading_7_600,
-} from "./styles/GlobalStyle";
+import { Router } from "./routes";
 
 function App() {
   return (
     <>
-      <Heading_1_700>Capstone M6</Heading_1_700>
-      <Heading_2_600>Capstone M6</Heading_2_600>
-      <Heading_3_600>Capstone M6</Heading_3_600>
-      <Heading_3_600>Capstone M6</Heading_3_600>
-      <Heading_4_600>Capstone M6</Heading_4_600>
-      <Heading_4_500>Capstone M6</Heading_4_500>
-      <Heading_5_600>Capstone M6</Heading_5_600>
-      <Heading_5_500>Capstone M6</Heading_5_500>
-      <Heading_6_600>Capstone M6</Heading_6_600>
-      <Heading_6_500>Capstone M6</Heading_6_500>
-      <Heading_7_600>Capstone M6</Heading_7_600>
-      <Heading_7_500>Capstone M6</Heading_7_500>
+      <Router />
     </>
   );
 }
