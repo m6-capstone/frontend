a315d808a788e2a40291e1040ce3e679eb7dbc07
Daniel Josias <daniieljosiaslima@gmail.com>
2022-12-17 21:49:06 -0300

    feat: o componente de informação do anunciante está feito

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Router } from "./routes";
-import Comments from "./components/Comments";
 
 function App() {
   return (
     <>
-      {/* <Router /> */}
-      <Comments/>
+      <Router />
     </>
   );
 }
