57c9b310ea3e49c93d3f27b88e39f36a84b1799e
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2022-12-19 12:47:45 -0300

    feat: alteração no Theme Global

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,4 +1,27 @@
 import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+   * {
+       margin: 0;
+       padding: 0;
+       outline: 0;
+       box-sizing:border-box;
+       font-family: 'Open Sans', sans-serif; 
+   }
+
+   #root {
+       margin: 0 auto;
+   }
+
+   ul, li, button {
+    all: unset;
+   }
+
+   button {
+    cursor: pointer;
+   }
+`;
 
 export const Heading_1_700 = styled.h1`
   color: ${(props) => props.theme.colors.brand.brand1};
@@ -58,7 +81,7 @@ export const Heading_7_600 = styled.h1`
 export const Heading_7_500 = styled.h1`
   color: ${(props) => props.theme.colors.random.random10};
   ${(props) => props.theme.typography.heading["heading-7-500"]}
-`
+`;
 
 export const Text_body_1_400 = styled.p`
   color: ${(props) => props.theme.colors.grey.grey9};
