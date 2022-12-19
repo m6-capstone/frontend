ad6e03be3d8f71ac11ecdf42a9b17c051a2c30b7
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2022-12-19 15:03:16 -0300

    feat: criação do Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,33 +3,51 @@ import {
   HeaderContainer,
   HeaderItem,
   ImgLogo,
-  LoginBar,
   Menu,
   NavBar,
 } from "./style";
 import Logo from "../../assets/motors-shop.svg";
 import { Button } from "../Button";
+import { useMediaQuery } from "usehooks-ts";
+import { MdMenu, MdClose } from "react-icons/md";
+import { useState } from "react";
+import { MenuMobile } from "../MenuMobile";
 
 export const Header = () => {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <HeaderContainer>
-      <ImgLogo src={Logo} />
-      <NavBar>
-        <Menu>
-          <HeaderItem>Carros</HeaderItem>
-          <HeaderItem>Motos</HeaderItem>
-          <HeaderItem>Leilão</HeaderItem>
-        </Menu>
-        <LoginBar>
-          <Bar />
-          <HeaderItem>Fazer Login</HeaderItem>
-          <Button
-            textStyle="button-big-text"
-            content="Cadastrar"
-            borderColor="grey4"
-          />
-        </LoginBar>
-      </NavBar>
-    </HeaderContainer>
+    <>
+      <HeaderContainer>
+        <ImgLogo src={Logo} />
+        {isDesktop ? (
+          <NavBar>
+            <Menu>
+              <HeaderItem>Carros</HeaderItem>
+              <HeaderItem>Motos</HeaderItem>
+              <HeaderItem>Leilão</HeaderItem>
+              <Bar />
+              <HeaderItem>Fazer Login</HeaderItem>
+              <Button
+                textStyle="button-big-text"
+                content="Cadastrar"
+                borderColor="grey4"
+              />
+            </Menu>
+          </NavBar>
+        ) : (
+          <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            {isMenuOpen ? (
+              <MdClose size={"1.5em"} />
+            ) : (
+              <MdMenu size={"1.5em"} />
+            )}
+          </button>
+        )}
+      </HeaderContainer>
+      {isMenuOpen && !isDesktop && <MenuMobile />}
+    </>
   );
 };
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -2,12 +2,17 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   height: 80px;
-  padding: 0 60px;
+  padding: 0 16px;
+  width: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
   border: 1px solid ${(p) => p.theme.colors.grey6};
   background-color: ${(p) => p.theme.colors.grey10};
+
+  @media (min-width: 768px) {
+    padding: 0 60px;
+  }
 `;
 
 export const LogoContainer = styled.div``;
@@ -18,11 +23,29 @@ export const NavBar = styled.nav`
   display: flex;
   align-items: center;
   gap: 44px;
+  flex-direction: column;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
 `;
 
 export const Menu = styled.ul`
   display: flex;
   gap: 44px;
+  flex-direction: column;
+  padding: 32px 0;
+
+  button {
+    text-align: center;
+  }
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    align-items: center;
+
+    padding: unset;
+  }
 `;
 
 export const HeaderItem = styled.li`
@@ -37,10 +60,4 @@ export const Bar = styled.div`
   height: 80px;
 
   background-color: ${(props) => props.theme.colors.grey6};
-`;
-
-export const LoginBar = styled.ul`
-  display: flex;
-  align-items: center;
-  gap: 44px;
 `;
