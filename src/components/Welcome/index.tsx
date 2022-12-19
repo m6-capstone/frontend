4623e33d75a287c267fa6d368ba415dd132c8a0c
Daniel Josias <daniieljosiaslima@gmail.com>
2022-12-19 16:35:05 -0300

    fix: refatoração da tela welcome feita

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -1,17 +1,17 @@
-import { Container } from "./style" 
+import { WelcomeContainer, TitleContainer,Title, Text, ButtonContainer, Button } from "./style" 
 
 export default function Welcome() {
   return (
-    <Container>
-        <div className="welcome">
-            <h2 className='welcome__title'>Velocidade e experiência em um lugar feito para você</h2>
-            <p className='welcome__paragraph'>Um ambiente feito para você explorar o seu melhor</p>
-            
-            <div className="welcome__container">
-                <button className='welcome__button'>Carros</button>
-                <button className='welcome__button'>Motos</button>
-            </div>
-        </div>
-    </Container>
+    <WelcomeContainer>
+      <TitleContainer>
+        <Title>Velocidade e experiência em um lugar feito para você</Title>
+        <Text>Um ambiente feito para você explorar o seu melhor</Text>
+      </TitleContainer>
+      
+      <ButtonContainer>
+        <Button>Carros</Button>
+        <Button >Motos</Button>
+      </ButtonContainer>
+    </WelcomeContainer>
   )
 }
