15cada4d0cc4460794d865f957bffb72df80db4c
Daniel Josias <daniieljosiaslima@gmail.com>
2022-12-19 13:32:11 -0300

    fix: a tela de welcome foi refatorada

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import Welcome from "./components/Welcome";
 import { Router } from "./routes";
 
 function App() {
   return (
     <>
-      {/* <Router /> */}
-      <Welcome/>
+      <Router />
+
     </>
   );
 }
