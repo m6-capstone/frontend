856b77e112037f4a4a04e79563463a9c2ecdbf5c
Daniel Josias <daniieljosiaslima@gmail.com>
2022-12-19 18:32:21 -0300

    fix: a refatoração do componente de informações do anunciante está feito

--- src/components/AdvertiserInformation/style.ts
+++ src/components/AdvertiserInformation/style.ts
@@ -4,78 +4,64 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     gap: 20px;
-    background-color: ${(props) => props.theme.colors.grey.white};
+    padding: 40px 40px;
+    background-color: ${(props) => props.theme.colors.white};
+`
 
-    .advertiser__initials{
-        display: flex;
-        text-align:  center;
-        align-items: center;
-        justify-content: center;
-        font-family: 'Inter';
-        font-style: normal;
-        font-weight: 500;
-        line-height: 53px;
-        font-size: 36px;
-        color: ${(props) => props.theme.colors.grey.white};
-        background-color: ${(props) => props.theme.colors.random.random9};
-        display: flex;
-        text-align:  center;
-        align-items: center;
-        justify-content: center;
-        border-radius: 50px;
-        width: 104px;
-        height: 104px;
-    }
-    
-    .advertiser__container{
-        display: flex;
-        align-items: center;
-        gap: 10px;
-    }
+export const Initials = styled.div`
+    display: flex;
+    text-align:  center;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50px;
+    width: 104px;
+    height: 104px;  
+    color: ${(props) => props.theme.colors.white};
+    background-color: ${(props) => props.theme.colors.random9};
+`
 
-    .advertiser__name{
-        font-family: 'Lexend';
-        font-style: normal;
-        font-weight: 600;
-        font-size: 20px;
-        line-height: 25px;
-        color: ${(props) => props.theme.colors.grey.grey1};
-    }
-    
-    .advertiser__label{
-        font-family: 'Inter';
-        font-style: normal;
-        font-weight: 500;
-        font-size: 14px;
-        line-height: 24px;
-        padding: 5px;
-        background-color: ${(props) => props.theme.colors.brand.brand4};
-        color: ${(props) => props.theme.colors.brand.brand1};
-    }
-    
-    .advertiser__about{
-        font-family: 'Inter';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 16px;
-        line-height: 28px;
-        color: ${(props) => props.theme.colors.grey.grey2};
-    }
+export const AdvertiserContainer = styled.div`
+    display: flex;  
+    align-items: center;
+    gap: 10px;
+`
 
-    .advertiser__button{
-        font-family: 'Inter';
-        font-style: normal;
-        font-weight: 600;
-        font-size: 16px;
-        line-height: 0px;
-        width: 160px;
-        height: 48px;
-        border: 1.5px solid ${(props) => props.theme.colors.brand.brand1};
-        background-color: transparent;
-        color: ${(props) => props.theme.colors.brand.brand1};
-    }
+export const Name = styled.span`
+    color: ${(props) => props.theme.colors.grey1};
+    ${(props) => props.theme.typography.heading["heading-6-600"]}
+`
 
-    .advertiser__button:hover{
+export const Label = styled.span`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 4px 8px;
+    gap: 10px;
+    background-color: ${(props) => props.theme.colors.brand4};
+    color: ${(props) => props.theme.colors.brand1};
+    ${(props) => props.theme.typography.text["body-2-500"]}
+`
+
+export const AboutMe = styled.p`
+    color: ${(props) => props.theme.colors.grey2};
+    ${(props) => props.theme.typography.text["body-1-400"]}
+
+`
+
+export const Button = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    width: 160px;
+    height: 48px;
+    border: 1.5px solid ${(props) => props.theme.colors.brand1};
+    background-color: transparent;
+    color: ${(props) => props.theme.colors.brand1};
+    ${(props) => props.theme.typography.text["button-big-text"]}
+
+    &:hover{
         cursor: pointer;
     }
 `
