e65e8aea75624bfe2259724d77e77608dd24eff9
Everton1~ <bio.e.s.o@gmail.com>
2022-12-21 10:33:35 -0300

    feat: input inplement

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,20 @@
 import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
 
 export const Home = () => {
   return (
     <>
       <Header />
+      <Input 
+        label="Nome"
+        type="text"
+        placeholder="Digite seu nome"
+        width="default"
+        defaultValue=""
+        register={{ name: "name" }}
+        error={undefined}
+        className=""
+      />
     </>
   );
 };
