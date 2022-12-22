f4272ac7c758a54969fa5911e09f39e4fb543d19
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2022-12-22 16:02:30 -0300

    feat: page Products

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@
 
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
+import { Product } from "../pages/Product";
 import { ProfileViewAdmin } from "../pages/ProfileViewAdmin";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/product" element={<Product />} />
       <Route path="/profileviewadmin" element={<ProfileViewAdmin />} />
     </Routes>
   );
